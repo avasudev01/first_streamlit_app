6e5f8db906c704c4ea89476b4a943f0c2bcb7406
avasudev01 <139165957+avasudev01@users.noreply.github.com>
2023-07-10 15:16:18 -0400

    Update streamlit_app.py
    add pandas library and import csv file with steamlit.dataframe function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas
 
 streamlit.title('My parent healthy diner')
 
@@ -7,3 +8,6 @@ streamlit.header('😊😊😊😊Breakfast menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale , Spinash & Rocket smoothie')
 streamlit.text('Hard Boiled Free range egg')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
