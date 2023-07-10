a500c64fbd3d35bdf67097f4887256872d40b580
avasudev01 <139165957+avasudev01@users.noreply.github.com>
2023-07-10 15:04:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 streamlit.title('My parent healthy diner')
 
 streamlit.header('Breakfast menu')
-steamlit.text('Omeg 3 & Blueberry oatmeal')
+steamlit.text("Omega 3 & Blueberry oatmeal")
 steamlit.text('Kale , Spinash & Rocket smoothie')
 steamlit.text('Hard Boiled Free range egg')
